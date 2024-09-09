057dac25637261bf009da6c1494073c79dae43f9
Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>
2024-09-09 23:29:26 +0700

    feat: date time picker by  Expansion-panel component

---
+++ graceful-florist-web/src/app/components/graceful-florist-date-time-picker/graceful-florist-date-time-picker.component.ts
@@ -0,0 +1,50 @@
+import {Component, ViewChild, signal} from '@angular/core';
+import {ThemePalette} from '@angular/material/core';
+import {MatChipListboxChange} from '@angular/material/chips';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MatExpansionPanel} from '@angular/material/expansion';
+
+export interface ChipColor {
+  name: string;
+  color: ThemePalette;
+}
+
+@Component({
+  selector: 'graceful-florist-date-time-picker',
+  templateUrl: './graceful-florist-date-time-picker.component.html',
+  styleUrl: './graceful-florist-date-time-picker.component.css'
+})
+export class GracefulFloristDateTimePickerComponent {
+  @ViewChild(MatExpansionPanel) panel!: MatExpansionPanel;
+  readonly panelOpenState = signal(false);
+  selected: Date | null = null;
+  availableColors: ChipColor[] = [
+    {name: 'Buổi Sáng (8:30 ~ 12:00)', color: 'primary'},
+    {name: 'Buổi Chiều (12:00 ~ 17:00)', color: 'primary'},
+    {name: 'Buổi Tối (17:00 ~ 20:30)', color: 'primary'},
+    {name: 'Giao Hàng Nhanh (1-2 giờ)', color: 'primary'},
+    {name: 'Tùy Chọn', color: 'primary'}
+  ];
+  hours: number[] = Array.from({length: 24}, (_, i) => i);
+  minutes: number[] = Array.from({length: 60}, (_, i) => i);
+  readonly range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null)
+  });
+  selectedChip: string | null = null;
+
+  selectChip(chip: MatChipListboxChange): void {
+    this.selectedChip = chip.value;
+  }
+
+  formatDate(date: Date | null): string {
+    if (date) {
+      return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+    }
+    return 'Chọn ngày';
+  }
+
+  closePanel(): void {
+    this.panel.close();
+  }
+}
