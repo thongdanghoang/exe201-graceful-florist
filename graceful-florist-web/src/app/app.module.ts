97d9adcc01f4910310ce5c672cd067a42061d87d
Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>
2024-09-09 23:31:46 +0700

    feat: import commons angular material ui modules
    Signed-off-by: Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>

--- graceful-florist-web/src/app/app.module.ts
+++ graceful-florist-web/src/app/app.module.ts
@@ -1,15 +1,43 @@
 import {NgModule} from '@angular/core';
-import {MatSlideToggle} from '@angular/material/slide-toggle';
 import {BrowserModule} from '@angular/platform-browser';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {GracefulFloristDateTimePickerComponent} from './components/graceful-florist-date-time-picker/graceful-florist-date-time-picker.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, MatSlideToggle],
-  providers: [provideAnimationsAsync()],
+  declarations: [AppComponent, GracefulFloristDateTimePickerComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatChipsModule,
+    MatCardModule
+  ],
+  providers: [provideAnimationsAsync(), provideNativeDateAdapter()],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
