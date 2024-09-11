01781bc7265af6046b3355f20e40a1a7f2cbb34b
Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>
2024-09-12 00:12:12 +0700

    feat: a part of homepage.component.html

--- graceful-florist-web/src/app/app.module.ts
+++ graceful-florist-web/src/app/app.module.ts
@@ -11,6 +11,7 @@ import {SharedModule} from './modules/shared/shared.module';
 import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {HomepageComponent} from './homepage/homepage.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,13 @@ import {HomepageComponent} from './homepage/homepage.component';
     HomepageComponent,
     FooterComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    NgOptimizedImage
+  ],
   providers: [provideAnimationsAsync(), provideNativeDateAdapter()],
   bootstrap: [AppComponent]
 })
