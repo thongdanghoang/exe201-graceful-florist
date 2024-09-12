dae5def5845b2b5aa399e286851925541a7dc139
Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>
2024-09-13 00:18:41 +0700

    feat: fix lint

--- graceful-florist-web/src/app/header/header.component.ts
+++ graceful-florist-web/src/app/header/header.component.ts
@@ -8,11 +8,10 @@ import {AppRoutingConstants} from '../app-routing-constants';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  protected readonly AppRoutingConstants = AppRoutingConstants;
   constructor(private readonly router: Router) {}
 
   navigate(path: string): void {
     void this.router.navigate([path]);
   }
-
-  protected readonly AppRoutingConstants = AppRoutingConstants;
 }
