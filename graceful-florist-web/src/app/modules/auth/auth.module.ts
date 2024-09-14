04bb7a583088df7442d537d1798eecb70aa6db8c
Thống ĐẶNG HOÀNG <work@thongdanghoang.id.vn>
2024-09-14 20:07:09 +0700

    bugfix: wrong service name
    Signed-off-by: Thống ĐẶNG HOÀNG <thongdh3401@gmail.com>

--- graceful-florist-web/src/app/modules/auth/auth.module.ts
+++ graceful-florist-web/src/app/modules/auth/auth.module.ts
@@ -2,12 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AuthRoutingModule} from './auth-routing.module';
 import {SharedModule} from '../shared/shared.module';
-import {ApplicationService} from './services/application.service';
+import {AuthService} from './services/auth.service';
 import {AuthorizeComponent} from './components/authorize/authorize.component';
 
 @NgModule({
   declarations: [AuthorizeComponent],
   imports: [CommonModule, AuthRoutingModule, SharedModule],
-  providers: [ApplicationService]
+  providers: [AuthService]
 })
 export class AuthModule {}
